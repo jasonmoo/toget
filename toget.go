79b82fdb39f6bf20f3e21f82bb3a925ba7d71d45
Jason Mooberry <jason.mooberry@gmail.com>
2014-11-11 16:38:49 -0500

    More helpers and test

--- toget.go
+++ toget.go
@@ -2,29 +2,80 @@ package toget
 
 import (
 	"errors"
+	"io"
 	"net/http"
 	"time"
 )
 
-type response struct {
-	resp *http.Response
-	err  error
-}
-
 var TimeoutError = errors.New("http get timeout")
 
-func Get(url string, to time.Duration) (*http.Response, error) {
+func Head(url string, timeout time.Duration) (*http.Response, error) {
+
+	req, err := http.NewRequest("HEAD", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return Do(req, timeout)
+
+}
+
+func Get(url string, timeout time.Duration) (*http.Response, error) {
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return Do(req, timeout)
+
+}
+
+func Post(url string, body io.Reader, timeout time.Duration) (*http.Response, error) {
+
+	req, err := http.NewRequest("POST", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return Do(req, timeout)
+
+}
+
+func Put(url string, body io.Reader, timeout time.Duration) (*http.Response, error) {
+
+	req, err := http.NewRequest("PUT", url, body)
+	if err != nil {
+		return nil, err
+	}
+
+	return Do(req, timeout)
+
+}
+
+func Delete(url string, timeout time.Duration) (*http.Response, error) {
+
+	req, err := http.NewRequest("DELETE", url, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return Do(req, timeout)
+
+}
+
+func Do(req *http.Request, timeout time.Duration) (*http.Response, error) {
+
+	type response struct {
+		resp *http.Response
+		err  error
+	}
 
 	var (
 		tr     = &http.Transport{}
 		client = &http.Client{Transport: tr}
 		ret    = make(chan response, 1)
 	)
-
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return nil, err
-	}
 
 	go func() {
 		resp, err := client.Do(req)
@@ -34,7 +85,7 @@ func Get(url string, to time.Duration) (*http.Response, error) {
 	select {
 	case r := <-ret:
 		return r.resp, r.err
-	case <-time.After(to):
+	case <-time.After(timeout):
 		tr.CancelRequest(req)
 		return nil, TimeoutError
 	}
